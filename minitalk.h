9dbad4d816ccfa0c4057b68f6409f21afb37dcd7
dinoguei <dinoguei@c1r8s13.42porto.com>
2023-01-18 12:42:06 +0000

    Header

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dinoguei <dinoguei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 17:17:11 by dinoguei          #+#    #+#             */
+/*   Updated: 2023/01/16 15:39:24 by dinoguei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include "libft/include/libft.h"
+
+unsigned char	*ft_strjoinserver(unsigned char *str, unsigned char c);
+
+#endif
